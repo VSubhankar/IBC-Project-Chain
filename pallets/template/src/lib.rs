b4625586fa0247495ac50f0d6879578fb3709ac3
Subhankar Venkumhananti <95838403+VSubhankar@users.noreply.github.com>
2023-11-09 19:47:43 +0530

    Update pallet/lib.rs

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,108 +1,140 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#[allow(deprecated)]
+#[warn(dead_code)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
-
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-pub mod weights;
-pub use weights::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    use frame_support::traits::{Currency, Randomness};
+    // The basis which we build
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// Type representing the weight of this pallet
-		type WeightInfo: WeightInfo;
-	}
+    // Allows easy access our Pallet's `Balance` type. Comes from `Currency` interface.
+    type BalanceOf<T> =
+        <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
-	}
+    // Struct for holding Keymap information
+    #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen, Copy)]
+    #[scale_info(skip_type_params(T))]
+    pub struct Keymap<T: Config> {
+        // Using 16 bytes to represent a Keymap filename
+        pub filename: [u8; 16],
+        pub index_of_sha: [u8; 16],
+        // `None` assumes not for sale
+        pub owner: T::AccountId,
+    }
 
-	// Errors inform users that something went wrong.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-	}
+    /// Keeps track of the number of AccMappings in existence.
+    #[pallet::storage]
+    pub(super) type CountForMappings<T: Config> = StorageValue<_, u64, ValueQuery>;
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
+    /// Maps the Keymap struct to the Keymap filename.
+    #[pallet::storage]
+    pub(super) type AccMappings<T: Config> = StorageMap<_, Twox64Concat, [u8; 16], Keymap<T>>;
 
-			// Update storage.
-			<Something<T>>::put(something);
+    /// Track the AccMappings owned by each account.
+    #[pallet::storage]
+    pub(super) type AccMappingsOwned<T: Config> = StorageMap<
+        _,
+        Twox64Concat,
+        T::AccountId,
+        BoundedVec<[u8; 16], T::MaxAccMappingsOwned>,
+        ValueQuery,
+    >;
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
+    // Your Pallet's configuration trait, representing custom external types and interfaces.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::call_index(1)]
-		#[pallet::weight(T::WeightInfo::cause_error())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+        /// The Currency handler for the AccMappings pallet.
+        type Currency: Currency<Self::AccountId>;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
-	}
+        /// The maximum amount of AccMappings a single account can own.
+        #[pallet::constant]
+        type MaxAccMappingsOwned: Get<u32>;
+
+    }
+
+    // Your Pallet's events.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A new Keymap was successfully created.
+        Created { filename: [u8; 16], owner: T::AccountId },
+    }
+
+    // Your Pallet's error messages.
+    #[pallet::error]
+    pub enum Error<T> {
+        DuplicateKeymap,
+        TooManyOwned,
+        Overflow,
+    }
+
+    // Your Pallet's callable functions.
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Create a new unique Keymap.
+        #[pallet::weight(0)]
+        pub fn create_Keymap(
+            origin: OriginFor<T>,
+            filename: [u8; 16],
+            index_of_sha: [u8; 16],
+        ) -> DispatchResult {
+            // Make sure the caller is from a signed origin
+            let sender = ensure_signed(origin)?;
+
+            // Write new Keymap to storage by calling helper function
+            Self::append_mapping(&sender, filename, index_of_sha)?;
+
+            Ok(())
+        }
+    }
+
+    // Your Pallet's internal functions.
+    impl<T: Config> Pallet<T> {
+        // Helper to mint a Keymap
+        pub fn append_mapping(
+            curr_owner: &T::AccountId,
+            curr_filename: [u8; 16],
+            curr_index_of_sha: [u8; 16],
+        ) -> Result<[u8; 16], DispatchError> {
+            // Create a new object
+            let keymap_obj = Keymap {
+                owner: curr_owner.clone(),
+                filename: curr_filename,
+                index_of_sha: curr_index_of_sha,
+            };
+            
+
+            // Check if the Keymap does not already exist in our storage map
+            ensure!(!AccMappings::<T>::contains_key(&keymap_obj.filename), Error::<T>::DuplicateKeymap);
+
+            // Performs this operation first as it may fail
+            let count = CountForMappings::<T>::get();
+            let new_count = count.checked_add(1).ok_or(Error::<T>::Overflow)?;
+
+            // Append Keymap to AccMappingsOwned
+            AccMappingsOwned::<T>::try_append(&curr_owner, keymap_obj.filename)
+                .map_err(|_| Error::<T>::TooManyOwned)?;
+
+            // Write new Keymap to storage
+            AccMappings::<T>::insert(keymap_obj.filename, keymap_obj);
+            CountForMappings::<T>::put(new_count);
+
+            // Deposit our "Created" event.
+            Self::deposit_event(Event::Created { filename: curr_filename, owner: curr_owner.clone()});
+
+            // Returns the filename of the new Keymap if this succeeds
+            Ok(curr_filename)
+        }
+    }
 }
